9df05528662fafe414c48882cf1ff1f35877b081
zainabjh01 <91522375+zainabjh01@users.noreply.github.com>
2024-09-12 16:08:04 +0200

    added routing

---
+++ frontend/src/views/PaymentPage.tsx
@@ -0,0 +1,18 @@
+// Import necessary modules from React
+import React from 'react';
+
+// Define the component as a functional component
+const PaymentPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My Simple Page</h1>
+            <p>This is a simple page created with TypeScript and React.</p>'
+            <a href={"http://localhost:3000/users/"}>tryk</a>
+
+        </div>
+    );
+}
+    ;
+
+    // Export the component for use in other parts of your application
+export default PaymentPage;
