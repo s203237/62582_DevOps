9a1506e6dc03e8e821833d05d080e23bcdf9d89b
Magnus Ottosen <magnus@ottosen.dk>
2024-10-10 11:25:19 +0200

    Super simple server

--- backend/src/server.py
+++ backend/src/server.py
@@ -3,21 +3,29 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Connect to the SQLite database in the new 'database/' folder
-def connect_db():
-    conn = sqlite3.connect('database/mydatabase.db')
-    return conn
+# # Connect to the SQLite database in the new 'database/' folder
+# def connect_db():
+#     conn = sqlite3.connect('database/mydatabase.db')
+#     return conn
 
-# Example route to fetch items from the database
-@app.route('/items', methods=['GET'])
-def get_items():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM items")
-    items = cursor.fetchall()
-    conn.close()
+# # Example route to fetch items from the database
+# @app.route('/items', methods=['GET'])
+# def get_items():
+#     conn = connect_db()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM items")
+#     items = cursor.fetchall()
+#     conn.close()
     
-    return jsonify(items)
+#     return jsonify(items)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
